2da59f9ef71b77e6a12da309e19e75ce881a16f7
CAgudeloBetancur <camiloab97@gmail.com>
2025-01-04 19:25:02 -0500

    feat: dashboard layout | sidebar partial view | option by role partial Views
    - Create dashboard layout
    - Create sidebar partial view
    - Create sidebar options partial views by roles

---
+++ HotelABC/wwwroot/js/dashboard.js
@@ -0,0 +1,12 @@
+﻿$(() => {
+
+    const $sidebar = $("#sidebar");
+    const $toggleButton = $("#sidebarToggle");
+    const $contentWrapper = $("#content-wrapper");
+
+    $toggleButton.on("click", () => {
+        $sidebar.toggleClass("collapsed");
+        $contentWrapper.toggleClass("expanded");
+    });
+
+});
